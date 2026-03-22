0d7a31dc83ffc4ac7361bd64a0f622e72c0329cc
DanderBot <lukedanderz@gmail.com>
2026-03-22 15:39:12 +0000

    Update build date to 2026-03-21

--- Changelog.lua
+++ Changelog.lua
@@ -1,5 +1,5 @@
 local addonName, DF = ...
-DF.BUILD_DATE = "2026-03-20T15:16:39Z"
+DF.BUILD_DATE = "2026-03-21T14:05:54Z"
 DF.RELEASE_CHANNEL = "alpha"
 DF.CHANGELOG_TEXT = [===[
 # DandersFrames Changelog
